adbf1a7229028dc83287ad7a68ece5b98c22f772
psk <psk3060@naver.com>
2018-01-22 09:22:12 +0900

    Build

--- singletest1/src/com/anttest/test/TestEmployeeDetails.java
+++ singletest1/src/com/anttest/test/TestEmployeeDetails.java
@@ -1,7 +1,7 @@
 package com.anttest.test;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import com.anttest.model.Employee;
 import com.anttest.business.EmpBusiness;
