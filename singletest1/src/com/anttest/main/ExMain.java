856dccd5adad205250bc58c3ba44d94d95b85953
psk <psk3060@naver.com>
2018-01-26 17:24:30 +0900

    SONAR

--- singletest1/src/com/anttest/main/ExMain.java
+++ singletest1/src/com/anttest/main/ExMain.java
@@ -5,6 +5,8 @@ import com.anttest.util.*;
 
 public class ExMain {
 	public static void main(String... args) {
+		int i = 1;
+		i = 1;
 		Employee employee = new Employee();
 		employee.setAge(15);
 		ObjectUtil.fixNullAndTrim(employee);
